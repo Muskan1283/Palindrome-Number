f6e1885d11692dff31175d4b53eb5b8d66fa9668
Muskan Sagar <99795889+Muskan1283@users.noreply.github.com>
2022-04-07 13:13:33 +0530

    Add files via upload

---
+++ Palindrome Number 2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum = 0, temp;
+            Console.WriteLine("Enter the number:");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while (n > 0)
+            {
+                r = n % 10;
+                sum = (sum * 10) + r;
+                n = n / 10;
+            }
+            if (temp == sum)
+                Console.WriteLine("Number is Palindrome.");
+            else
+                Console.WriteLine("Number is not palindrome:");
+
+        }
+    }
+}
